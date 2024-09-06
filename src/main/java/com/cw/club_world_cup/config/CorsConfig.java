5f80d2ee8924cdef7fbf7fc788833f69467ce323
Wael Rezgui <wael.rezgui@esen.tn>
2024-09-06 01:05:16 +0100

    Update CorsConfig.java

--- src/main/java/com/cw/club_world_cup/config/CorsConfig.java
+++ src/main/java/com/cw/club_world_cup/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://club-world-cup-2025.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
