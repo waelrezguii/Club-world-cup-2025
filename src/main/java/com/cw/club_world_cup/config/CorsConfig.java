39d6f0f98e2b81a009f776465961a8a3e2ee27d7
Wael Rezgui <wael.rezgui@esen.tn>
2024-09-08 16:38:01 +0100

    Update CorsConfig.java

--- src/main/java/com/cw/club_world_cup/config/CorsConfig.java
+++ src/main/java/com/cw/club_world_cup/config/CorsConfig.java
@@ -1,16 +1,19 @@
 package com.cw.club_world_cup.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-  @Override
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("https://club-world-cup-2025.vercel.app")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("*")
-            .allowCredentials(true);
+                .allowedOrigins("https://club-world-cup-2025.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true);
     }
 }
